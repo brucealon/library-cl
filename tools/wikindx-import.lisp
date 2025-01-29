25d0318ecc0f68e582fe8c71dbb6d756c58ea8dd
Bruce King <bruce@minuteproductions.com>
2025-01-28 19:21:29 -0800

    Beginning WikiNDX files

--- tools/wikindx-import.lisp
+++ tools/wikindx-import.lisp
@@ -7,7 +7,10 @@
                   :user *mysql-user*
                   :password *mysql-password*)
 
-(let* ((result-set (cl-mysql:query "select r.resourceType as resource_type,
+(let* ((result-set (cl-mysql:query "select r.resourceId as id,
+                                           m.resourcemiscPublisher as publisher,
+                                           m.resourcemiscCollection as collection,
+                                           r.resourceType as resource_type,
                                            r.resourceTitle as title,
                                            r.resourceSubtitle as subtitle,
                                            r.resourceShortTitle as shorttitle,
@@ -16,18 +19,22 @@
                                            r.resourceField4 as volumenumber,
                                            r.resourceField6 as pages,
                                            r.resourceIsbn as isbn,
-                                           c.creatorFirstname as author_first,
-                                           c.creatorInitials as author_middle,
-                                           c.creatorSurname as author_last,
                                            coalesce(t.resourcetextUrls, '') as url,
                                            m.resourcemiscField2 as pub_day,
                                            m.resourcemiscField3 as pub_month,
-                                           m.resourcemiscField4 as volumes
+                                           m.resourcemiscField4 as volumes,
+                                           rq.resourcequotePageStart as quote_start,
+                                           rq.resourcequotePageEnd as quote_end,
+                                           rqt.resourcequotetextText as quote,
+                                           rqc.resourcequotecommentComment as quote_comment
                                       from WKX_resource r
                                         inner join WKX_resource_misc m on r.resourceId = m.resourcemiscId
-                                        left outer join WKX_resource_creator rc on r.resourceId = rc.resourcecreatorResourceId
+                                        left outer join WKX_resource_creator rc on rc.resourcecreatorResourceId = r.resourceId
                                         left outer join WKX_creator c on rc.resourcecreatorCreatorId = c.creatorId
-                                        left outer join WKX_resource_text t on r.resourceID = t.resourcetextId"))
+                                        left outer join WKX_resource_text t on r.resourceID = t.resourcetextId
+                                        left outer join WKX_resource_quote rq on rq.resourcequoteResourceId = r.resourceId
+                                        left outer join WKX_resource_quote_comment rqc on rqc.resourcequotecommentQuoteId = rq.resourcequoteId
+                                        left outer join WKX_resource_quote_text rqt on rqt.resourcequotetextId = rq.resourcequoteId"))
        (books   (first  (first result-set)))
        (headers (second (first result-set))))
-  headers)
+  (first books))
