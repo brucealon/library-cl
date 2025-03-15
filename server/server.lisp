01856a89dca32ba741a684a04f4752642c6a2fc8
Bruce King <bruce@minuteproductions.com>
2025-03-15 13:48:03 -0700

    Add djula, templates, use db macro

--- server/server.lisp
+++ server/server.lisp
@@ -1,6 +1,7 @@
 
 (ql:quickload :hunchentoot)
 (ql:quickload :easy-routes)
+(ql:quickload :djula)
 
 (load (merge-pathnames ".library-config.lisp" (user-homedir-pathname)))
 (load (merge-pathnames "libs/library-database.lisp" *project-dir*))
@@ -8,22 +9,34 @@
 (defparameter *server* nil)
 (defparameter *server-port* 4242)
 
+(djula:add-template-directory (merge-pathnames "html/" *project-dir*))
+
 (easy-routes:defroute root ("/") ()
-  "hello app")
+  (djula:render-template* (djula:compile-template* "library.html")))
 
 (easy-routes:defroute route-publications ("/publications") ()
-  (with-connection (list *postgres-db* *postgres-user* *postgres-password* *postgres-host* :pooled-p t)
-    (with-output-to-string (out)
-      (loop for publication being the elements of (all-publications) do
-        (format out "Publication: ~a<br/>~%" (second publication)))
-      out)))
+  (with-library-db
+    (djula:render-template* (djula:compile-template* "publications.html")
+                            nil
+                            :publications (all-publications))))
+
+(easy-routes:defroute route-publication ("/publications/:publication") ()
+  (with-library-db
+    (djula:render-template* (djula:compile-template* "publication.html")
+                            nil
+                            :publication (first (publication-by-id :id publication)))))
 
 (easy-routes:defroute route-creators ("/creators") ()
-  (with-connection (list *postgres-db* *postgres-user* *postgres-password* *postgres-host* :pooled-p t)
-    (with-output-to-string (out)
-      (loop for creator being the elements of (all-creators) do
-        (format out "Creator: ~a ~a<br/>~%" (second creator) (third creator)))
-      out)))
+  (with-library-db
+    (djula:render-template* (djula:compile-template* "creators.html")
+                            nil
+                            :creators (all-creators))))
+
+(easy-routes:defroute route-creator ("/creators/:creator") ()
+  (with-library-db
+    (djula:render-template* (djula:compile-template* "creator.html")
+                            nil
+                            :creator (first (creator-by-id :id creator)))))
 
 (if (not (null *server*))
     (format nil "Need to stop the current server first?")
